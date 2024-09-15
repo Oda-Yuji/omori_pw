44824dd2e8ed4f5ef6218ea02988d0144c8b83f0
Oda-Yuji <yujioda1703@gmail.com>
2024-09-15 18:25:18 -0300

    sm

--- javas/1.js
+++ javas/1.js
@@ -1,14 +1,29 @@
 function show() 
 {
+    document.body.style.overflowY = "hidden";
+    document.getElementById('cg')
+        .style.height = "100vh";
+    document.getElementById('cg')
+        .style.minHeight = "100vh";
+    document.getElementById('cg')
+        .style.maxHeight = "100vh";
     document.getElementById('preto')
         .style.display = "flex";
     document.getElementById('algo')
         .style.display = "flex";
     document.getElementById('logo')
         .style.display = "none";
+    
 }
 function unshow() 
 {
+    document.body.style.overflowY = "scroll";
+    document.getElementById('cg')
+        .style.height = "100%";
+    document.getElementById('cg')
+        .style.minHeight = "100vh";
+    document.getElementById('cg')
+        .style.maxHeight = "100%";
     document.getElementById('preto')
         .style.display = "none";
     document.getElementById('algo')
@@ -16,11 +31,14 @@ function unshow()
     document.getElementById('logo')
         .style.display = "flex";
 }
-function unshow2() 
+(function(window)
 {
-    document.getElementById('porta')
-        .style.display = "none";
-    document.getElementById('trailer')
-        .style.display = "block";
-    document.getElementById('trailer').play();
-}
+    function video()
+    {
+        var v = document.getElementById('cole');
+        v.addEventListener('mouseover', function() {this.controls = true}, false);
+        v.addEventListener('mouseout', function() {this.controls = false}, false);
+    }
+
+    window.addEventListener('load', video, false);
+})(window)
